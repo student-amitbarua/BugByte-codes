fca6b706b172d33ca7235de96f6a9abc84cd3314
Amit Barua <amitbarua737@gmail.com>
2026-03-03 22:28:27 +0600

    vid 1 & 2 added

--- django_practice/starter_code/settings.py
+++ django_practice/starter_code/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'django_extensions'
+    'django_extensions',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
