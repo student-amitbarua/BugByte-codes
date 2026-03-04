43f1c3a8bacc2380e07d0c659b0b1e3d8e983ca3
Amit Barua <amitbarua737@gmail.com>
2026-03-04 20:20:33 +0600

    vid 4 & 5 uploaded

--- django_practice - 3/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:pk>/', views.product_Detail),
-]
--- django_practice -3/api/views.py
+++ django_practice -3/api/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
+from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
+from django.db.models import Max
 
 
 # def product_list(request):
@@ -29,4 +30,20 @@ def product_list(request):
 def product_Detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)   #we don't need all data thats the reason i remove the many = true
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'Products' : products,
+        'count': len(products),
+        'max_price': products.aggregate(max_price= Max('price')) ['max_price']
+    })
     return Response(serializer.data)
