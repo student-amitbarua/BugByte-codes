23152805ca44fb251f3701ce26dda6251da7765d
Amit Barua <amitbarua737@gmail.com>
2026-03-06 09:18:00 +0600

    10 uploaded

---
+++ django_practice - 10/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('products/', views.ProductListCreateAPIView.as_view()),
+    # path('products/create/', views.ProductCreateAPIView.as_view()),
+    # path('products/', views.product_list),
+    # path('products/info/', views.product_info),
+    path('products/info/', views.ProductInfoAPIView.as_view()),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),
+    # path('products/<int:pk>/', views.product_Detail),
+    path('orders/', views.OrderListAPIView.as_view()),
+    path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
+
+    # path('orders/', views.order_list)
+]
