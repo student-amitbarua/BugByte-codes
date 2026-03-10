e41e84c22c634bdcd4e68034de9582429c126797
Amit Barua <amitbarua737@gmail.com>
2026-03-10 22:31:58 +0600

    vid 29 added

---
+++ django_practice -28/api/views.py
@@ -0,0 +1,140 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from api.serializers import (ProductSerializer, OrderSerializer, ProductInfoSerializer, OrderCreateSerializer, UserSerializer)
+from rest_framework.decorators import action
+from api.models import Product, Order, OrderItem, User
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from django.db.models import Max
+from django.views.decorators.vary import vary_on_cookie, vary_on_headers
+from rest_framework import generics
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAdminUser,
+    AllowAny)
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from api.filters import ProductFilter, InStockFilterBackend, OrderFilter
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.throttling import ScopedRateThrottle
+
+
+# use isort for orgainized the line and file structure
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    throttle_scope = 'products'
+    throttle_classes = [ScopedRateThrottle]
+    queryset = Product.objects.order_by('pk') 
+    serializer_class = ProductSerializer
+    filterset_class = ProductFilter
+    filter_backends = [
+        DjangoFilterBackend, 
+        filters.SearchFilter,
+        filters.OrderingFilter,
+        InStockFilterBackend,
+        ]
+    search_fields = ['name', 'description'] # if use '=name ' then results outcome will be exact word 
+    ordering_fields = ['name', 'price', 'stock']
+    pagination_class = None
+    # pagination_class = PageNumberPagination
+    # pagination_class.page_size = 2 # directly override and refresh the page2
+    # pagination_class.page_query_param = 'pagenum' #url e custom pagenum ta set kora 
+    # pagination_class.page_query_param = 'size' # you can use  ?size=1 or 2 and ...
+    # pagination_class.max_page_size = 4
+
+    @method_decorator(cache_page(60 * 15, key_prefix='product_list'))
+    def list(self, request, *args, **kwargs):
+        return super(). list(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        import time
+        time.sleep(2)
+        return super().get_queryset()
+
+    
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == "POST":
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
+    
+
+
+class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
+
+    def get_permissions(self):
+        
+        self.permission_classes = [AllowAny]
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
+
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    throttle_scope = 'orders'
+    queryset = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
+    filterset_class = OrderFilter
+    filter_backends = [DjangoFilterBackend]
+
+    def get_serializer_class(self):
+        # can also check if POST: if self.request.method == "POST"
+        if self.action == 'create' or self.action == 'update':
+            return OrderCreateSerializer
+        return super().get_serializer_class()
+    
+    
+    @method_decorator(cache_page(60 * 15, key_prefix='order_list'))
+    @method_decorator(vary_on_headers("Authorization"))
+    def list(self, request, *args, **kwargs):
+        return super(). list(request, *args, **kwargs)
+    
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if not self.request.user.is_staff: # staff false kora ase jate admin or stuff vad e kono customer others order dekte parve na
+            qs = qs.filter(user=self.request.user)
+        return qs
+
+    @action(
+        detail=False,
+        methods=['get'],
+        url_path='user-orders',
+    )
+    def user_orders(self, request):
+        orders = self.get_queryset().filter(user=request.user)
+        serializer = self.get_serializer(orders, many=True)
+        return Response(serializer.data)
+
+
+
+class ProductInfoAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer({
+            'products': products,
+            'count': len(products),
+            'max_price': products.aggregate(max_price=Max('price'))['max_price']
+        })
+        return Response(serializer.data)
+    
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    pagination_class = None
+
+
